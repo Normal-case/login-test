9938075486064a081958d1e2ab2f1a40d5945266
Normal-case <8282chan94@gmail.com>
2022-12-14 14:18:41 +0900

    fs register

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,14 +1,20 @@
 'use strict'
 
+const fs = require('fs').promises
+
 class UserStorage {
-    static #users = {
-        id: ['chan', 'bok', 'yeo'],
-        password: ['123', '1234', '1111'],
-        name: ['찬복', '여찬', '복여']
+    static #getUserInfo(data, id) {
+        const users = JSON.parse(data)
+        const idx = users.id.indexOf(id)
+        const usersKeys = Object.keys(users) // => [id, password, name]
+        const userInfo = usersKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx]
+            return newUser
+        }, {})
+        return userInfo
     }
 
     static getUsers = (...fields) => {
-        const users = this.#users
         const newUsers = fields.reduce((newUser, field) => {
             if (users.hasOwnProperty(field)) {
                 newUser[field] = users[field]
@@ -19,19 +25,14 @@ class UserStorage {
     }
 
     static getUserInfo(id) {
-        const users = this.#users
-        const idx = users.id.indexOf(id)
-        const usersKeys = Object.keys(users) // => [id, password, name]
-        const userInfo = usersKeys.reduce((newUser, info) => {
-            newUser[info] = users[info][idx]
-            return newUser
-        }, {})
-
-        return userInfo
+        return fs.readFile('./src/databases/loginApp/users.json')
+            .then((data) => {
+                return this.#getUserInfo(data, id)
+            })
+            .catch(console.error)
     }
 
     static save(userInfo) {
-        const users = this.#users
         users.id.push(userInfo.id)
         users.name.push(userInfo.name)
         users.password.push(userInfo.password)
